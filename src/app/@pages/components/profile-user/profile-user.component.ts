0be12c99b71f98b81c78c79a13bba5bba6f771ec
huytht <huytht2001@gmail.com>
2022-01-01 00:19:32 +0700

    show info when user login, update info user

--- src/app/@pages/components/profile-user/profile-user.component.ts
+++ src/app/@pages/components/profile-user/profile-user.component.ts
@@ -1,15 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AppUserService } from 'src/app/@services/app-user.service';
+import { AuthenticationService } from './../../../@services/authentication.service';
+import { finalize } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { UserInfo } from 'src/app/@model/user-info';
+import { AppUtilService } from './../../../@services/app-util.service';
 
 @Component({
   selector: 'app-profile-user',
   templateUrl: './profile-user.component.html',
   styleUrls: ['./profile-user.component.css']
 })
-export class ProfileUserComponent implements OnInit {
+export class ProfileUserComponent implements OnInit, OnDestroy {
+  
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  user: any;
+  activeEdit: boolean = false;
+  refreshing: boolean;
+  fileStatus: any;
+  progress: number;
+  profileImage: File;
+  
+  constructor(private authenticationService: AuthenticationService, private userService: AppUserService, private toastr: ToastrService, private appUtilService: AppUtilService) { }
 
   ngOnInit(): void {
+    this.user = this.authenticationService.getUserInfoFromLocalCache();
+  }
+
+  onActiveEdit(): void {
+    this.activeEdit = true;
+  }
+
+  onCancelEdit(): void {
+    this.activeEdit = false;
+  }
+
+  updateProfileImage(): void {
+    document.getElementById("profile-image-input").click();
+  }
+
+  onEditUser(ngForm: NgForm): void {
+    this.refreshing = true;
+    const formData = this.userService.createUserFormData(this.user.userId, this.authenticationService.getLoggedInUsername(), ngForm.value);
+    let userUpdate = this.appUtilService.convertFormDataToJSON((formData));
+    userUpdate['userId'] = +userUpdate['userId'];
+    this.subscriptions.push(this.userService.updateUser(userUpdate)
+    .pipe(finalize(() => { this.refreshing = false; }))
+    .subscribe(
+      (response: any) => {
+        userUpdate['avatarImg'] = this.user.avatarImg;
+        this.authenticationService.addUserInfoToLocalCache(userUpdate);
+        this.toastr.success(`${userUpdate.username} has been updated successfully`)
+        this.activeEdit = false;
+      }, (error: any) => {
+        this.toastr.error(error.error.errorMessage);
+      }
+    ));
+  }
+
+  onProfileImageChange(event: any): void {
+    this.profileImage = event.target.files[0];
+  }
+
+  onUpdateProfileImage(): void {
+    const formData = new FormData();
+    this.progress = 0;
+    formData.append('profileImage', this.profileImage);
+
+    this.subscriptions.push(this.userService.updateProfileImage(formData)
+        .pipe(finalize(() => {this.refreshing = false; this.profileImage = null; }))
+        .subscribe(
+          (event: any) => {
+            console.log(event);
+                if (event.type === HttpEventType.UploadProgress) {
+                  this.progress = Math.round(100 * event.loaded / event.total);
+                } else if (event.type === HttpEventType.Response) {
+                  if (event.status === 200) {
+                    this.user.avatarImg = `${event.body.data}?time=${new Date().getTime()}`;
+                    this.appUtilService.addToLocalCache("user", this.user);
+                    this.toastr.success('Image has been updated successfully');
+                  } else {
+                    this.toastr.error(`Unable to upload profile image. Please try again`);
+                  }
+                }
+              }, error => this.toastr.error(error.error.errorMessage)
+    ));
+  }
+
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
--- src/app/@pages/shared/header/header.component.ts
+++ src/app/@pages/shared/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationService } from 'src/app/@services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  
+  
+  constructor(public authenticationService: AuthenticationService, private router: Router, private toastr: ToastrService ) { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    this.authenticationService.logOut();
+    this.router.navigateByUrl('/signin').then(r => this.toastr.info('You have logged out successfully'));
   }
 
 }
