8a25380c7d8a1418bf86798c9200579ff8ad729f
huytht <huytht2001@gmail.com>
2022-03-03 15:17:38 +0700

    add filter album, track by genre and category

--- src/app/@pages/components/genre/genre.component.ts
+++ src/app/@pages/components/genre/genre.component.ts
@@ -1,15 +1,165 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Album } from 'src/app/@model/album';
+import { AlbumService } from 'src/app/@services/album.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonService } from 'src/app/@services/common.service';
+import { Subscription, BehaviorSubject } from 'rxjs';
+import { Track } from './../../../@model/track';
+import { TrackService } from 'src/app/@services/track.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { AppUtilService } from './../../../@services/app-util.service';
 
 @Component({
   selector: 'app-genre',
   templateUrl: './genre.component.html',
-  styleUrls: ['./genre.component.css']
+  styleUrls: [
+    './genre.component.css',
+  ],
 })
-export class GenreComponent implements OnInit {
+export class GenreComponent implements OnInit, OnDestroy {
+  
+  private subscriptions: Subscription[] = [];
+  defaultImage: string = 'assets/images/default-image.png';
 
-  constructor() { }
+  albums: Album[] = [];
+  thePageNumber: number;
+  thePageSize: number;
+  theTotalElements: number;
+
+  tracks: Track[] = [];
+  thePageNumberTrack: number;
+  thePageSizeTrack: number;
+  theTotalElementsTrack: number;
+
+  currentCategoryId: number;
+  previousCategoryId: number;
+  currentCategoryName: string;
+
+  currentGenreId: number;
+  previousGenreId: number;
+  currentGenreName: string;
+
+  chooseOptionTrack: boolean[] = [];
+
+  constructor(
+    private albumService: AlbumService,
+    public trackService: TrackService,
+    public appUtilService: AppUtilService,
+    private route: ActivatedRoute,
+    public commonService: CommonService
+  ) {}
 
   ngOnInit(): void {
+    // this.thePageNumber = 1;
+    this.thePageSize = 1;
+    this.theTotalElements = 0;
+
+    this.thePageNumberTrack = 1;
+    this.thePageSizeTrack = 5;
+    this.theTotalElementsTrack = 0;
+
+    this.route.paramMap.subscribe(() => {
+      this.listAlbum();
+      this.listTrack();
+    });
+
+    this.commonService.getCurrentCategory().subscribe((response: any) => {
+      this.currentCategoryName = response;
+    });
+
+    this.commonService.getCurrentGenre().subscribe((response: any) => {
+      this.currentGenreName = response;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  openChooseOptionTrack(index): void {
+    this.chooseOptionTrack[index] = !this.chooseOptionTrack[index];
+  }
+
+  listAlbum() {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('category-id');
+    const hasGenreId: boolean = this.route.snapshot.paramMap.has('genre-id');
+
+    if (hasCategoryId && hasGenreId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('category-id');
+      this.currentGenreId = +this.route.snapshot.paramMap.get('genre-id');
+
+      if (
+        this.currentCategoryId !== this.previousCategoryId ||
+        this.currentGenreId !== this.previousGenreId
+      ) {
+        this.albums = [];
+        this.theTotalElements = 0;
+        this.thePageNumber = 1;
+      }
+
+      this.previousCategoryId = this.currentCategoryId;
+      this.previousGenreId = this.currentGenreId;
+
+      this.albumService
+        .listAlbumByCategoryAndGenre(
+          this.currentCategoryId,
+          this.currentGenreId,
+          this.thePageNumber - 1,
+          this.thePageSize
+        )
+        .subscribe((response: any) => {
+          this.albums = response.data.content;
+          this.thePageNumber = response.data.pageInfo.currentPage + 1;
+          this.thePageSize = response.data.pageInfo.pageSize;
+          this.theTotalElements = response.data.pageInfo.totalElements;
+        });
+    }
+  }
+
+  listTrack() {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('category-id');
+    const hasGenreId: boolean = this.route.snapshot.paramMap.has('genre-id');
+
+    if (hasCategoryId && hasGenreId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('category-id');
+      this.currentGenreId = +this.route.snapshot.paramMap.get('genre-id');
+
+      if (
+        this.currentCategoryId !== this.previousCategoryId ||
+        this.currentGenreId !== this.previousGenreId
+      ) {
+        this.tracks = [];
+        this.theTotalElementsTrack = 0;
+        this.thePageNumberTrack = 1;
+      }
+
+      this.previousCategoryId = this.currentCategoryId;
+      this.previousGenreId = this.currentGenreId;
+
+      this.trackService
+        .listTrackByCategoryAndGenre(
+          this.currentCategoryId,
+          this.currentGenreId,
+          this.thePageNumberTrack - 1,
+          this.thePageSizeTrack
+        )
+        .subscribe((response: any) => {
+          // console.log(response.data.content);
+          this.tracks = response.data.content === null ? [] : response.data.content;
+          this.thePageNumberTrack = response.data.pageInfo.currentPage + 1;
+          this.thePageSizeTrack = response.data.pageInfo.pageSize;
+          this.theTotalElementsTrack = response.data.pageInfo.totalElements;
+        });
+    }
+  }
+
+  changePageAlbum(event: any) {
+    this.thePageNumber = event;
+    this.listAlbum();
+  }
+
+  changePageTrack(event: any) {
+    this.thePageNumberTrack = event;
+    this.listTrack();
+  }
 }
